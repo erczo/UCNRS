ee8a2d0c776c1d7b8f2f46587a08bfb9f605b590
Collin <collin@Mimsy.local>
2017-07-24 11:16:41 -0700

    Pseudobinary converter
    Updated code and added documentation.  Now exports to csv.

--- pseudobinary_converter.js
+++ pseudobinary_converter.js
@@ -1,11 +1,14 @@
 // pseudobinary_converter.js
 /*
-Pseudo-binary Translation into a normal number
+Campbell Scientific data loggers use the GOES satellite system to transmit
+from remote locations.  To keep transmissions to a minimum, they use pseudobinary.
+Pseudobinary is ASCII characters that can be decoded into normal numbers. 
+
+PSEUDOBINARY
 The number is reconstructed from three characters.  Each character is
-converted into 6 binary bits.  ASCII bits have more than 6 bits,
-Some of which are used for transmission or are unknown, so only the last 
-6 are used. If a string cannot be divided into sets of 3 characters, this
-translation cannot work.  
+converted into 6 binary bits.  ASCII characters can have from 6 to 11 bits.
+Only the last 6 bits are used. If a string cannot be divided into sets of 3 
+characters, this translation cannot work.  
 
 The pseudobinary converts to 3 parts: sign, exponent, and mantissa. 
 The 18 bits are used as follows:
@@ -16,16 +19,15 @@ bits	Function
         This is translated into 1,0.1,0.01,0.001 
 5-18		Mantissa:  bit list which is powers of 2, then added up. 2^bitpower
 				12 bits with the first the highest power and last the lowest.	
-*/
-// accept an argument
-if (process.argv.length <= 2) {
-    console.log("Usage: " + __filename + " SOME_PARAM");
-    process.exit(-1);
-}
-var param = process.argv[2];
-console.log(__filename+' param: ' + param);
 
-var test_string = 'BEC0035C17197002104G49-0NN301WXW01046 B^h@YJF@[F@\F@[F@@F@qF@rF@qF@@F@@F@@F@@F@@F@@F@@DjqDj]DjgEMTEIiEKt@OqF@@F@@I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~DT^DT]DT]DlHD{XGmXChvB^h@Y@F@\F@]F@\F@@F@sF@uF@rF@@F@@F@@F@@F@@F@@F@@DjgDjLDjWEOUEKZEME@OqF@@F@@I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~DT_DT]DT_Dl@D{XGmXChvB^h@XNF@]F@^F@]F@@F@wF@xF@uF@@F@@F@@F@@F@@F@@F@@DjRDipDj@ERPEN_EPd@OrF@@F@@I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~DT`DT_DT`DkzD{XGmXChvB^h@XDF@]F@^F@\F@AF@vF@xF@tF@@F@@F@@F@@F@@F@@F@@Di{DifDioETpEPSERy@OrF@@F@@I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~DT`DT_DT`DkvD{XGmXChvB^h@WzF@\F@]F@\F@@F@tF@uF@rF@@F@@F@@F@@F@@F@@F@@DjHDimDizEUgEQ|ESn@OrF@@F@@I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~DT_DSGDTZDk|D{XGmXChvB^h@WpF@^F@^F@]F@@F@xF@xF@uF@@F@@F@@F@@F@@F@@F@@DjgDi{DjUEVSER[ETP@OrF@@F@@I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~DT_DTMDT^DlED{XGmXChv ';
+DCP METADATA
+Each transmission is prefixed with a DCP transmission metadata string.  These
+should always be 37 characters long and are not in pseudobindary.  This must be
+parsed and separated from the message before any conversion can take place.  
+NOTE: the data does not have timestamps.  It must be interpreted from the medata 
+timestamp. 
+*/
+
 
 function DCPstring2array(lrgs) {
 	dcp_parts = [];
@@ -83,10 +85,10 @@ function pseudostring2array(pbstring) {
 		//console.log(i+' '+bits+' '+bits.length);
 
 		//bits ='012345678901234567'; // test srting
-		bitsign = Number(bits.substring(2,3));
-		bitdecimal1 = Number(bits.substring(3,4));
-		bitdecimal2 = Number(bits.substring(4,5));
-		bitmantissa = bits.substring(5,18);
+		var bitsign = Number(bits.substring(2,3));
+		var bitdecimal1 = Number(bits.substring(3,4));
+		var bitdecimal2 = Number(bits.substring(4,5));
+		var bitmantissa = bits.substring(5,18);
 		//console.log(i+' sign: '+bitsign+', decimals: '+bitdecimal1+', '+bitdecimal2+', mant: '+bitmantissa);
 
 		// Convert from bits to meaningful number parts
@@ -111,47 +113,71 @@ function pseudostring2array(pbstring) {
 	return values;
 }
 
+function values2data(goes_array,value_set,column_num) {
+	var data_rows = [];
+	// Timestamp - this is the time of transmission, so it is the last timestamp
+	// convert to ISO standard time UTC, provide offset. 
+
+	// Separate into data rows.  For hourly transmissions this should be 6 rows. 
+	// Do I assume hourly?  Calculate?  Input as argument? Default hourly.
+
+	// Row Timestamps - back calculate the time for each row, assuming interval based 
+	// on transmission frequency and the number of rows.  Default 10 min.
+
+	// Nan Values:   convert -8190 values to NaNs.  
+	
+	return data_rows;
+}
+
+//
 // Test the Pseudo-bindary conversion function
-/*
-console.log('String Length: '+lrgs_string.length);
-var pbstr_mod = lrgs_string.length % 3;
-console.log('Modulus of 3: '+pbstr_mod);
-*/
-var lrgs_string = [];
+//
+// Argv: Accept an argument
+if (process.argv.length <= 2) {
+    console.log("Usage: " + __filename + " SOME_PARAM");
+    process.exit(-1);
+}
+var param = process.argv[2];
+console.log(__filename+' param: ' + param);
+
+// Argv: command line argument can be test, debug, or a file name
+var lrgs_strings = [];
+var column_num = 58;
 if(param == 'test' || param == 'debug') {
-	lrgs_string.push(test_string);
+	column_num = 58;
+	var test_string = 'BEC0035C17197002104G49-0NN301WXW01046 B^h@YJF@[F@\F@[F@@F@qF@rF@qF@@F@@F@@F@@F@@F@@F@@DjqDj]DjgEMTEIiEKt@OqF@@F@@I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~DT^DT]DT]DlHD{XGmXChvB^h@Y@F@\F@]F@\F@@F@sF@uF@rF@@F@@F@@F@@F@@F@@F@@DjgDjLDjWEOUEKZEME@OqF@@F@@I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~DT_DT]DT_Dl@D{XGmXChvB^h@XNF@]F@^F@]F@@F@wF@xF@uF@@F@@F@@F@@F@@F@@F@@DjRDipDj@ERPEN_EPd@OrF@@F@@I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~DT`DT_DT`DkzD{XGmXChvB^h@XDF@]F@^F@\F@AF@vF@xF@tF@@F@@F@@F@@F@@F@@F@@Di{DifDioETpEPSERy@OrF@@F@@I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~DT`DT_DT`DkvD{XGmXChvB^h@WzF@\F@]F@\F@@F@tF@uF@rF@@F@@F@@F@@F@@F@@F@@DjHDimDizEUgEQ|ESn@OrF@@F@@I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~DT_DSGDTZDk|D{XGmXChvB^h@WpF@^F@^F@]F@@F@xF@xF@uF@@F@@F@@F@@F@@F@@F@@DjgDi{DjUEVSER[ETP@OrF@@F@@I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~I~DT_DTMDT^DlED{XGmXChv ';
+	lrgs_strings.push(test_string);
 	console.log(param);
 } 
-else {
-	// load file from argument
-	const fs = require('fs')
-	fs.readFile(param,'utf8', function(err,data) {
-		if(err) {
-			return console.log(err);
-		}
-		//console.log(data);
-		const readline = require('readline');
-		const rl = readline.createInterface({
-		  input: fs.createReadStream(param)
-		});
-		rl.on('line', (line) => {
-		  console.log('Line from file:', line);
-		 	lrgs_string.push(line) 
-		});
-	});	
+else { 	// Argv: load file from argument
+	var fs = require('fs')
+	lrgs_strings = fs.readFileSync(param).toString().split("\n");
 }
 
 // MAIN
-var goes_array = DCPstring2array(lrgs_string);
-console.log("GOES Array:");
-console.log(goes_array);
-var message = goes_array["message"];
-console.log("message length: "+message.length);
-var value_set = pseudostring2array(message);
-console.log(value_set.toString());
+for (var lrgs_string of lrgs_strings) {
+	if(lrgs_string.length > 18) {
+		// Parse GOES metadata and separate out Message
+		var goes_array = DCPstring2array(lrgs_string);
+		console.log("GOES Array:");
+		console.log(goes_array);
+		
+		// Parse Message, i.e. the data content of transmission
+		var message = goes_array["message"];
+		console.log("message length: "+message.length);
+		var value_set = pseudostring2array(message);
+		//console.log(value_set.toString());
+		
+		// Data Row Constructor - separate rows of data and add timestamps.
+		var data_set = values2data(goes_array,value_set,column_num);
+		console.log(data_set.toString());
 
-// Export Message to file
-value_set.forEach(function(value){
-
+		// Export Message content to CSV file
+		require("fs").appendFile( 
+			"pbstring_js_out.csv", 
+			value_set.toString()+'\n',
+			function(err) { console.log(err ? "Error on export: "+err : "ok")}
+		);
+	}
 }
 console.log("DONE!");
