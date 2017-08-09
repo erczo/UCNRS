9a2b7ea09e007676017599b11dfeb4440ec1544d
cbode <collin@berkeley.edu>
2017-08-08 17:37:05 -0700

    dri dat duplicate remover done
    works as advertised.  fails on rancho marino, column change mid-way
    without detection.  Rancho needs manual adjustment.

--- sensor_ucnrs_dri_dat_duplicates_remover.py
+++ sensor_ucnrs_dri_dat_duplicates_remover.py
@@ -54,15 +54,17 @@ def file_firstdate(filepath,sep=',',ts_pos=0):
                 break
     return ts,i  
 
-path = '/Users/collin/Desktop/SensorDB_Duplicates/'
+#path = '/Users/collin/Desktop/SensorDB_Duplicates/'
+path = 'D:/sensor/UCNRS_Dups/'
 inpath = path+'source/'
 outpath = path+'new/'
 
 print('UCNRS Stations Last Seen:')
-with open('file_stats.csv','w') as fout:
-    fout.write('file,date_start,date_end,rows_ideal,rows_actual,diff_iva,rows_fixed,diff_ivf\n')
+with open(path+'file_stats.csv','w') as freport:
+    report_header = 'file,date_start,date_end,rows_ideal,rows_actual,diff_iva,rows_fixed,diff_ivf\n'
+    freport.write(report_header)
     j = 0
-    for file in os.listdir(path):
+    for file in os.listdir(inpath):
         #print('test',file)
         if(len(file.split('.')) == 0):
             pass
@@ -70,9 +72,14 @@ with open('file_stats.csv','w') as fout:
             fpath = inpath+file
             opath = outpath+file
             
-            # Find Start and end dates
-            date_end = ''
+            # Set date and row count variables
+            date_end = dt.datetime(1800,1,1)
             date_start = ''
+            actual_row_count = 0
+            ideal_row_count = 0
+            fixed_row_count = 0
+            header_row = 0
+            header_length = 0
             
             # Get Header and create replacement file
             with open(opath,'w') as fout:
@@ -83,46 +90,64 @@ with open('file_stats.csv','w') as fout:
                     actual_row_count = len(all_lines)
                     for line in all_lines:
                         values = line.split(',')
-                        str_time = values[ts_pos].replace('"','')
+                        str_time = values[0].replace('"','')
+                        if(str_time == "TIMESTAMP"):
+                            header_row = i
+                            print(i,'HEADER FIELDS')
                         try:
                             ts = dt.datetime.strptime(str_time,"%Y-%m-%d %H:%M:%S")
-                            print(i,'Found Timestamp!',ts)
-                            break
+                            if(header_length == 0):
+                                print(i,'Found Timestamp!',ts)
+                                header_length = i
+                            # Record End Date
+                            if(date_end < ts):
+                                date_end = ts
                         except:
-                            continue
+                            if(date_start == ''):
+                                print(i,'HEADER:',line)
+                                fout.write(line)
+                                
+                        # Record Start Date
+                        if(date_start == ''):
+                            date_start = ts
                         i += 1
-                        if(i == actual_row_count):
-                            
-                    
-            '''
-            # Count Rows
-            date_interval = date_end - date_start
-            logger_interval = dt.timedelta(minutes=10)
-            ideal_row_count = (date_interval / logger_interval) + header_count
-            actual_row_count = 0
-            with open(fpath) as f:
-                actual_row_count = len(f.readlines())
-            rows_diff = ideal_row_count - actual_row_count
-
-            # Get Header and create replacement file
-            with open(outpath,'w') as fout:
-                with open(fpath,'r') as f:
-                    k = 0
-                    for line in f.readlines():
                         
-            
-            
-            # Remove Duplicatesusing Pandas
-            #df = pd.DataFrame(fpath,skiprows=1,)
-            '''
-
-            station = file.split('_')[1].strip()
-            print(station+"\t\t Start:",date_start," End:",date_end," Iddeal Row Count:",ideal_row_count," Actual Row Count:",actual_row_count," Difference:",rows_diff)
-            outrow = [file,dt.datetime.strftime(date_start,"%Y-%m-%d %H:%M:%S"),dt.datetime.strftime(date_end,"%Y-%m-%d %H:%M:%S"),str(ideal_row_count),str(actual_row_count),str(rows_diff)]
+                # Count Rows
+                date_interval = date_end - date_start
+                logger_interval = dt.timedelta(minutes=10)
+                ideal_row_count = (date_interval / logger_interval) + header_length
+                diff_iva = ideal_row_count - actual_row_count
+    
+                # Remove Duplicates using Pandas
+                # skip rows actually allows for removing before and after header rows
+                # this snippet builds a list of rows to be removed
+                skips = []
+                for s in range(0,header_length):
+                    if(s > header_row):
+                        skips.append(s)
+                # Create Pandas DataFrame from faulty file
+                df = pd.read_csv(fpath,skiprows=skips,header=header_row,parse_dates=True,index_col=0)
+                dfu = df.drop_duplicates(keep='first')
+                dfu.to_csv(path+'dfu_temp.csv',header=False)
+                
+                # Feed cleaned non-duplicate dfu_temp.csv file into new output file
+                with open(path+'dfu_temp.csv','r') as ffu:
+                    ffu_lines = ffu.readlines()
+                    fixed_row_count = len(ffu_lines)+header_length
+                    for line in ffu_lines:
+                        fout.write(line)
+                # Delete temp file
+                os.remove(path+'dfu_temp.csv')
+                
+                # count rows of fixed versus ideal
+                diff_ivf = ideal_row_count - fixed_row_count
+                
+            outrow = [file,dt.datetime.strftime(date_start,"%Y-%m-%d %H:%M:%S"),dt.datetime.strftime(date_end,"%Y-%m-%d %H:%M:%S"),str(ideal_row_count),str(actual_row_count),str(diff_iva),str(fixed_row_count),str(diff_ivf)]
             str_outrow = ','.join(outrow)
-            fout.write(str_outrow+'\n')
+            freport.write(str_outrow+'\n')
+            print(report_header)
+            print(str_outrow)
             j += 1
-            If(j > 1):
-                break
 
 print('----------------------------')
+print('-----DONE!------------------')
